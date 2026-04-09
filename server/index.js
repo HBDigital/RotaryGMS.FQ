d9ca54277565c89f455683f0ecb27e1e7d6ad5f5
infovivek <vivek@warblerit.com>
2026-04-09 18:11:48 +0530

    security: move admin auth to DB and remove frontend hardcoded credentials

--- server/index.js
+++ server/index.js
@@ -8,6 +8,8 @@ const adminRoutes = require('./routes/admin');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.disable('x-powered-by');
+
 app.use(cors({
   origin: [
     process.env.FRONTEND_URL || 'http://localhost:3000',
@@ -19,6 +21,13 @@ app.use(cors({
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  if (/(^|\/)\.[^/]+/.test(req.path)) {
+    return res.status(404).send('Not found');
+  }
+  next();
+});
 
 app.use('/api', registrationRoutes);
 app.use('/api', adminRoutes);
